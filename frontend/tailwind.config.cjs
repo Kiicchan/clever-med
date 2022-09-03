fc434ccfc503fca1267d5569eef5597a62a85f59
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-03 02:03:54 -0300

    configure colors

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -8,8 +8,33 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif']
-      }
+      },
     },
+    colors: {
+      background: '#F7FAFC',
+      gradient: {
+        blue: '#1482FC',
+        purple: '#9513FB'
+      },
+      white: '#FFF',
+      black: '#000',
+      shades: {
+        100: '#E7F0FD',
+        600: '#425466',
+        700: '#32323C',
+        800: '#27272E',
+        900: '#1A202C'
+      },
+      red: '#FC395C',
+      blue: {
+        100: '#D1DAFA',
+        300: '#1F80B7',
+        500: '#3962FC',
+        700: '#384FA1',
+      },
+      purple: '#9A39FC'
+
+    }
   },
   plugins: [],
 }
