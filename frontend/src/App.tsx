0e77c3c719b2b8412c67ac72f7a39b962a21ece3
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-04 00:38:47 -0300

    adjust image size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Aside } from "./components/Aside"
 function App() {
 
   return (
-    <div className="min-h-screen grid grid-cols-[1fr,1.42fr]">
+    <div className="min-h-screen grid grid-cols-[1fr,1.9fr]">
       <Aside />
     </div>
   )
--- frontend/src/components/Aside.tsx
+++ frontend/src/components/Aside.tsx
@@ -3,15 +3,13 @@ import Img from "../assets/fitness-woman.png"
 
 export function Aside() {
     return (
-        <div className="relative">
-            <img src={Img} alt="Exercising woman" className="absolute bottom-0 mix-blend-multiply" />
-            <div className="h-full bg-gradient-to-b from-gradient-blue to-gradient-purple">
-                <header className="p-10">
-                    <Logo />
-                </header>
-                <h1 className="text-3xl text-white font-bold text-left leading-tight mx-20">Relatórios de saúde em gráficos</h1>
-                <p className="text-lg text-white font-regular text-left mt-6 max-w-xs ml-auto mr-5">Em poucos segundos, transforme os seus dados de saúde em gráficos de fácil leitura.</p>
-            </div>
+        <div className="relative h-full bg-gradient-to-b from-gradient-blue to-gradient-purple">
+            <header className="p-10">
+                <Logo />
+            </header>
+            <h1 className="text-3xl text-white font-bold text-left leading-tight mx-20">Relatórios de saúde em gráficos</h1>
+            <p className="text-lg text-white font-regular text-left max-w-[60%] ml-auto mt-6 mr-5">Em poucos segundos, transforme os seus dados de saúde em gráficos de fácil leitura.</p>
+            <img src={Img} alt="Exercising woman" className="absolute bottom-0 max-w-[40%] max-h-[65%] mix-blend-multiply" />
         </div>
     )
 }
