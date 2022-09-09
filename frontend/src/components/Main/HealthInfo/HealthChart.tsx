471eeb3b4bd13fe30aa42df29a74660718ee50a1
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-09 17:41:55 -0300

    Create health Chart

--- frontend/src/components/Main/HealthInfo/HealthChart.tsx
+++ frontend/src/components/Main/HealthInfo/HealthChart.tsx
@@ -10,20 +10,23 @@ interface HealthChartProps {
 }
 export function HealthChart(props: HealthChartProps) {
     const [viewMode, setViewMode] = useState<'BPM' | 'PA'>('BPM')
+
     const measuredAtDate = new Date(props.metrics[0]?.measuredAt).toLocaleDateString()
-    const chartData = [
-        ["Year", "Sales", "Expenses"],
-        ["2004", 1000, 400],
-        ["2005", 1170, 460],
-        ["2006", 660, 1120],
-        ["2007", 1030, 540],
-    ]
+
+    let chartData: (number | string | Date)[][]
+    if (viewMode === 'BPM') {
+        chartData = props.metrics.map(item => [new Date(item.measuredAt), item.heartRateBPM])
+        chartData.unshift(['hora', 'Batimento Cardíaco'])
+    } else {
+        chartData = props.metrics.map(item => [new Date(item.measuredAt), item.bloodPressureHigh, item.bloodPressureLow])
+        chartData.unshift(['hora', 'Pressão Sistólica', 'Pressão Diastólica'])
+    }
 
     return (
         <div>
             <p className="text-sm font-semibold text-shades-600 text-center">Diário de Saúde</p>
             <p className="text-sm font-regular text-shades-600 text-center">{measuredAtDate}</p>
-            <div className="flex gap-2 my- [&>*]:min-w-[6.25rem]">
+            <div className="flex gap-2 mt-4 [&>*]:min-w-[6.25rem]">
                 <button onClick={() => setViewMode('BPM')}
                     className={`flex items-center gap-2 p-2 rounded font-bold active:scale-95 transition-transform
                             ${viewMode === 'BPM' ?
@@ -45,8 +48,15 @@ export function HealthChart(props: HealthChartProps) {
                 <Chart
                     chartType="LineChart"
                     width="100%"
-                    height="400px"
+                    height="300px"
                     data={chartData}
+                    options={{
+                        legend: { position: "bottom" },
+                        hAxis: {
+                            format: 'HH:mm',
+                            ticks: [...props.metrics.map(metric => new Date(metric.measuredAt))]
+                        },
+                    }}
                 />
             </div>
         </div>
