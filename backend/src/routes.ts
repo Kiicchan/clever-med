324a444d267f91c936e2c0f46c757d7c1156e1ba
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-10 02:41:15 -0300

    removing test routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,34 +1,8 @@
 import { Router } from "express";
-import { prisma } from "./db";
 import { AddHealthMetricsController } from "./useCases/AddHealthMetrics/AddHealthMetricsController";
 const router = Router()
 const addHealthMetricsController = new AddHealthMetricsController()
 
 router.post('/metrics', addHealthMetricsController.handle)
-
-router.get('/metrics', async (req, res) => {
-    try {
-        const allMetrics = await prisma.healthMetrics.findMany()
-        return res.status(200).send(allMetrics)
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.get('/patients', async (req, res) => {
-    try {
-        const allMetrics = await prisma.patient.findMany()
-        return res.status(200).send(allMetrics)
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.get('/', (request, response) => {
-    return response.status(200).send('OK')
-})
-router.post('/', (request, response) => {
-    return response.status(200).send(request.body)
-})
 
 export { router }
