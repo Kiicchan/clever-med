e03c913ef2288739a3eda9772e21d15b1549cdc5
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-11 17:45:57 -0300

    Creating CORS and .env settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,9 @@ import cors from 'cors'
 import { router } from './routes'
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    "origin": process.env.APP_URL,
+}))
 
 app.use(express.json())
 app.use(router)
