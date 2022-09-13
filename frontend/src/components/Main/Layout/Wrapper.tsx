67c375ba5574a61082b0613694197912ad2d1b4c
Marcos Chacon <marcoschacon39@gmail.com>
2022-09-12 23:55:35 -0300

    Extend Main Area in middle screens

--- frontend/src/components/Main/Layout/Wrapper.tsx
+++ frontend/src/components/Main/Layout/Wrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Wrapper(props: PropsWithChildren) {
     return (
-        <main className="mx-auto md:max-w-xl my-10 px-2 lg:w-2/3 lg:px-0">
+        <main className="mx-auto max-w-xl my-10 px-2 w-full lg:w-3/4 lg:px-0">
             {props.children}
         </main>
     )
